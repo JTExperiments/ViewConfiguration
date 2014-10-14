05b1119c3dac41c53efae440cdd5bd17e76d74a2
James Tang <j@jamztang.com>
2014-10-14 17:53:22 +0800

    Initial Commit

---
+++ TestHandler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestHandler
+//
+//  Created by James Tang on 14/10/14.
+//  Copyright (c) 2014 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
